f661b1836ab6194dcea028495e78a4e3e5957252
lita :p <paperalbo@gmail.com>
2023-11-25 10:56:14 +0100

    redimensionamiento lienzo
    Se hace la primera redimensión del lienzo y las proporciones de las imágenes (prueba)

--- Ratones a los fogones/src/init.js
+++ Ratones a los fogones/src/init.js
@@ -1,7 +1,7 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800, //hay que cambiar el tamaño de acuerdo al tamaño TOTAL del nivel
-    height: 600, //hay que cambiar el tamaño de acuerdo al tamaño TOTAL del nivel
+    width: 1600, //hay que cambiar el tamaño de acuerdo al tamaño TOTAL del nivel
+    height: 364, //hay que cambiar el tamaño de acuerdo al tamaño TOTAL del nivel
 
     //COSAS DE LA ESCENA
     scene: {
@@ -17,15 +17,13 @@ var player1;
 
 function preload(){
     this.load.image('tile_pared', 'assets/tiles/Tiles_Pared.png');
-    this.load.image('mapa_recto_1600x182', 'assets/tiles/Tiles_mapa_recto_1600x182.png');
+    this.load.image('mapa', 'assets/tiles/mapa_recto_1600x182.png');
     this.load.spritesheet('raton_player_1','assets/ratones/Raton_Base_WALK_RIGHT_20x30.png',{ frameWidth: 20, frameHeight: 30 } );
 }
 
 function create(){
-
-    this.add.image(400,300,'tile_pared');
-
-    player1 = this.add.sprite(100, 450, 'raton_player_1'); //crea el sprite del player, como se ha creado a partir de físicas tiene un cuerpo de física dinámica por defecto
+    this.add.image(0,0,'mapa').setScale(2);
+    player1 = this.add.sprite(90, 145, 'raton_player_1').setScale(3); //crea el sprite del player, como se ha creado a partir de físicas tiene un cuerpo de física dinámica por defecto
 
 }
 
